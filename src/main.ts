22fba344961e4c951fca5f59835bddb2bf6bc900
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2023-10-08 01:27:07 +0100

    chore: init new nest app with auth module

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+
+const config = new ConfigService();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(config.get('APP_PORT'));
+  Logger.log(
+    `Application is running on: http://localhost:${config.get('APP_PORT')}`,
+  );
+}
+bootstrap();
